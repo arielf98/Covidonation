9ea6c37a6a80503990e7e697b471ea00b4788df3
Ariel <arielfebrian98@gmail.com>
2021-01-01 19:55:14 +0700

    penambahan ftiur kotak koleksi

---
+++ src/components/NotifComponent/UnderLine.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { styles } from '../../Style/NotifStyle'
+
+export default function UnderLine() {
+    
+        return(
+        < View style = { styles.notifBottomLine } >
+
+        </View >
+        )  
+}
---
+++ src/screens/Profile/DetailKotakKoleksi.js
@@ -0,0 +1,112 @@
+import React, {useContext, useEffect} from 'react'
+import { View, Text } from 'react-native'
+import { Card, Title, TouchableRipple, Appbar } from 'react-native-paper'
+import * as Parent from '../../Style/ParentStyle'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import UnderLine from '../../components/NotifComponent/UnderLine'
+import { useBackHandler } from '@react-native-community/hooks'
+import { store } from '../../Config/Contex/store'
+import { useNavigation } from '@react-navigation/native'
+import { styles } from '../../Style/ProfileStyle'
+import AttachmentCard from './AttachmentCard'
+import { ScrollView } from 'react-native-gesture-handler'
+
+
+
+
+export default function DetailKotakKoleksi({route}) {
+    const DATA = [
+        {
+            id: "1",
+            judulKonten: "Too Much to Ask",
+            namaKreator: "Niall Horan",
+            gambarKonten: "https://picsum.photos/700",
+        },
+        {
+            id: "2",
+            judulKonten: "Again",
+            namaKreator: "YUI",
+            gambarKonten: "https://picsum.photos/700",
+        },
+        {
+            id: "3",
+            judulKonten: "Stuck with U",
+            namaKreator: "Ariana Grande & Justin Bieber",
+            gambarKonten: "https://picsum.photos/700",
+        },
+    ]
+
+    const navigation = useNavigation()
+    const globalState = useContext(store)
+    const { dispatch } = globalState
+    const { namaKreator, judul, gambarKonten } = route.params
+
+
+    useBackHandler(() => {
+        dispatch({ type: 'IS_HIDE', payload: true })
+        navigation.goBack()
+        return true
+    })
+
+    
+
+    return (
+        <View>
+            <Appbar
+              style={{ backgroundColor: Parent.colorBlueMax }} >
+                <Appbar.BackAction onPress={ ()=> {
+                    navigation.goBack()
+                    dispatch({ type: 'IS_HIDE', payload: true }) 
+                    }} />
+                <Title style={{ color: 'white' }} > Kotak Koleksi </Title>
+            </Appbar>
+
+            <Card style={{ height: '100%' }} >
+                <Card.Content style={{ marginBottom: 10 }} >
+
+                    <Title
+                        style={styles.itemKoleksiTitle} > {judul} </Title>
+                    <Icon
+                        style={styles.itemKoleksiIcon}
+                        name="user-alt"
+                        size={15}
+                        color="#999999" />
+                    <Text
+                        style={styles.itemKoleksiAuthor} > {namaKreator} </Text>
+
+                    <Card.Cover style={{ width: 100, height: 100, borderRadius: 20 }}
+                            resizeMode='cover'
+                            source={{ uri: gambarKonten }}/>
+                </Card.Content>
+
+                {/* ------- untuk garis bawah ------ */}
+                <UnderLine/>
+
+                    <View style={{ marginHorizontal: 20, marginVertical: 15 }} >
+                        <Text style={{ fontSize: 18, fontWeight: '800' }} > 
+                        silahkan download buku panduan untuk mengikuti webinar ini  
+                        </Text>
+                    </View>
+
+                    {/* //TODO buatkan list untuk item yang dibuat oleh kreator */}
+                    
+                    <ScrollView 
+                        style={{ marginBottom: 50 }}
+                        showsVerticalScrollIndicator={false}     >
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                        <AttachmentCard/>
+                    </ScrollView>
+                    
+            </Card> 
+       
+        </View>
+    )
+}
