35534c0614c8395c13878bcc9da25563f98f5ff5
arirahmansyah <arirahmansyahputra8741@gmail.com>
2021-01-15 21:38:21 +0700

    Link navbar ke halaman media

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useState, useContext, useEffect } from 'react'
 import HomeScreen from './src/screens/Home/Home'
 import { ScreenSearch } from './src/screens/Search'
-import { BottomNavigation, Text } from 'react-native-paper';
+import { BottomNavigation } from 'react-native-paper';
 import Notif from './src/screens/Notifikasi/Notif';
 import { store } from './src/Config/Contex/store'
 import Profile from './src/screens/Profile/Profile';
 import * as Parent from './src/Style/ParentStyle'
+import Media from './src/screens/Media/Media';
 
 export default function App() {
 
@@ -18,15 +19,16 @@ export default function App() {
     const [routes] = useState([
       { key: 'home', title: 'Awal', icon: 'home', color: Parent.colorBlueMax },
       { key: 'search', title: 'Pencarian', icon: 'magnify', color: Parent.colorBlueMax },
+      { key: 'media', title: 'Tambah', icon: 'plus', color: Parent.colorBlueMax },
       { key: 'notification', title: 'Notifikasi', icon: 'bell', color: Parent.colorBlueMax  },
       { key: 'profile', title: 'Akun Saya', icon: 'account', color: Parent.colorBlueMax },
     ])
-  
 
     // setiap halaman akan di tampilkan disini.....
     const renderScene = BottomNavigation.SceneMap({
       home: HomeScreen,
       search: ScreenSearch,
+      media: Media,
       notification: Notif,
       profile: Profile,
     })
